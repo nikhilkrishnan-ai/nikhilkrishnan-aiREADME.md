c761c0b35b5846b504195329cf799d60b9a430be
Nikhil Krishnan <nikhilkr51@gmail.com>
2026-04-20 10:00:45 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,4 +2,12 @@ import functions_framework
 
 @functions_framework.http
 def analyze_jump_cloud(request):
-    return {"status": "Connected", "message": "NK, your GitHub link is working!"}, 200
+    request_json = request.get_json(silent=True)
+    
+    # We get the speed from the data sent to the URL
+    speed = request_json.get('speed', 0) if request_json else 0
+    
+    if speed > 47.5:
+        return {"status": "SPOOF_DETECTED", "speed": speed, "analyst": "NK"}, 200
+    else:
+        return {"status": "Clear", "speed": speed, "analyst": "NK"}, 200
