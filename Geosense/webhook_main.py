58f290362773e38b319656e0cecd7180d997cf93
nikhilkrishnan-ai <nikhilkr51@gmail.com>
2026-06-06 17:08:15 +0530

    Added Dockerfile to fix deployment

--- Geosense/webhook_main.py
+++ Geosense/webhook_main.py
@@ -26,8 +26,8 @@ async def receive_gps_data(request: Request, x_api_key: str = Header(None)):
     return {"status": "success", "message": "GeoSense Data securely received!"}
 
 if __name__ == "__main__":
-    #
-    # ഗൂഗിൾ തരുന്ന പോർട്ട് എടുക്കുക, അല്ലെങ്കിൽ ഡിഫോൾട്ട് ആയി 8080 ഉപയോഗിക്കുക
+    
+    # # ഗൂഗിൾ തരുന്ന പോർട്ട് എടുക്കുക, അല്ലെങ്കിൽ ഡിഫോൾട്ട് ആയി 8080 ഉപയോഗിക്കുക
     import os
     port = int(os.environ.get("PORT", 8080))
     uvicorn.run(app, host="0.0.0.0", port=port)
